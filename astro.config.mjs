80389937add8549c813de20bf8c06dc69d757f0d
deleonspower <140031157+deleonspower@users.noreply.github.com>
2023-07-23 11:22:23 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deleonspower.github.io',
-  base: '/',
+  base: '/site',
   integrations: [tailwind(), react()]
 });
